f136677631a7b883bec9b9ca0abd97053a906e5c
MATOO-Dev <fagoldob@gmail.com>
2023-05-16 19:51:07 +0200

    Added solution for task 26 + some comments

--- main.cpp
+++ main.cpp
@@ -31,13 +31,47 @@ v(n) wächst unbeschrankt, rechte seite konvergent gegen 0 -> falsch
 -> aus etwas wahrem etwas falsches folgern -> falsch
 */
 
+
+
+/*
+Task 26
+table:
+    insert:
+        keine schleifen -> ca. konstante laufzeit (n paar if/else)
+        daher O(1)
+        speicheraufwand: 1 node (1 * item_type, 2 * pointer)
+    delete:
+        keine schleifen -> ca. konstante laufzeit (n paar if/else)
+        daher O(1)
+        keine zusätzlichen speicheraufwände
+
+array:
+    insert:
+        for-loop von array-ende bis iterator -> potenziell n
+        daher O(n)
+        speicheraufwand: 1 array-eintrag (1 * item_type) + evtl array resize
+    delete:
+        for-loop von iterator bis array ende -> potenziell n 
+        daher O(n)
+        keine zusätzlichen speicheraufwände
+
+    => Doppelt verkettete list besitzt deutlich schnellere laufzeit für insert/delete
+    => Doppelt verkettete liste benutzt etwas mehr speicher pro eintrag (2 zusätzliche pointer)
+        -> relativ vernachlässigbar außer bei abertausenden pointern (beispiel int: 8 bytes pro eintrag)
+        -> bei implementation mit automatischem array-resize kann das array größer ausfallen als benötigt!
+*/
+
+
+
 int main()
 {
+    //create a new table
     Table<int> myTable = Table<int>();
 
     //extra space for valgrind and gdb readability;
     std::cout << "\n";
-    //get table size
+
+    //fill table with +4 to -4
     for(int i = 0; i < 5; i++)
     {
         myTable.insert(i);
@@ -46,10 +80,12 @@ int main()
     for(int i = -1; i > -5; i--)
         myTable.append(i);
 
+    //print out the initial state after filling
     std::cout << "initial state:\n";
     myTable.print();
     std::cout <<"\n";
 
+    //move back and forth a bit
     myTable.first();
     myTable.next();
     myTable.next();
@@ -57,29 +93,34 @@ int main()
     myTable.next();
     myTable.next();
     
+    //read a value and return it
     int container = 0;
     myTable.get_node(container);
     std::cout << "entry value is: " << container << std::endl;
 
+    //write a value, then read it to verify
     std::cout << "setting entry value to 27\n";
     container = 27;
     myTable.set_node(container);
     myTable.get_node(container);
     std::cout <<"\n";
-
     myTable.print();
+
+    //delete the first entry
     myTable.first();
     myTable.delete_node();
     std::cout <<"\n";
     std::cout << "deleting first entry\n";
-    
     myTable.print();
+    
+    //delete the last entry
     myTable.last();
     myTable.delete_node();
     std::cout <<"\n";
     std::cout << "deleting last entry\n";
-
     myTable.print();
+
+    //delete the middle entry (technically the fourth, but in this case middle)
     myTable.first();
     myTable.next();
     myTable.next();
@@ -87,8 +128,6 @@ int main()
     myTable.delete_node();
     std::cout <<"\n";
     std::cout << "deleting middle entry\n";
-    
-    std::cout << "final state:\n";
     myTable.print();
 
     //extra space for valgrind and gdb readability;
